1a65bb8fe8d3e89742ba18f03d5575208f10365c
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-05-24 22:45:04 +0200

    Revert "Update our_hangman.py"
    This reverts commit ab3145fbdd5638d8d23d70b94c46f97a9ac1f8d4.

--- our_hangman.py
+++ our_hangman.py
@@ -4,7 +4,8 @@ import pyfiglet
 import re
 import string
 
-class bcolors:
+
+class BColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -14,7 +15,9 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-#print(bcolors.FAIL + "Example text" + bcolors.ENDC)
+
+
+# print(BColors.FAIL + "Example text" + BColors.ENDC)
 
 HANGMAN = (
     """
@@ -29,7 +32,7 @@ HANGMAN = (
  |
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -41,7 +44,7 @@ HANGMAN = (
  |
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -53,7 +56,7 @@ HANGMAN = (
  |   
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -65,7 +68,7 @@ HANGMAN = (
  |   
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -77,7 +80,7 @@ HANGMAN = (
  |   
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -89,7 +92,7 @@ HANGMAN = (
  |   
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -101,7 +104,7 @@ HANGMAN = (
  |   
 ----------
 """,
-"""
+    """
  ------
  |    |
  |    O
@@ -114,15 +117,16 @@ HANGMAN = (
 ----------
 """)
 
-
 with open("words.txt") as f:
     words = f.read().splitlines()
+
 
 def get_name():
     player_name = input("Please enter your name. ")
     return player_name
 
-def get_level():  
+
+def get_level():
     print(" 1 - Easy")
     print(" 2 - Medium")
     print(" 3 - Hard")
@@ -136,7 +140,7 @@ def get_level():
         while level > 3 or level <= 0:
             level = int(input("You have chosen wrong level, please try again. "))
         break
-        
+
     if level == 1:
         print("You've chosen easy level. ")
     elif level == 2:
@@ -145,28 +149,29 @@ def get_level():
         print("You've chosen hard level. ")
     return int(level)
 
-def get_word(words, level):
 
-    word_number = random.randint(0, len(words) -1)
-    word = words[word_number].upper()
+def get_word(word_list, level):
+    word_number = random.randint(0, len(word_list) - 1)
+    word = word_list[word_number].upper()
 
     if level == 1:
         while len(word) > 5:
-            word_number = random.randint(0, len(words) -1)
-            word = words[word_number].upper()
+            word_number = random.randint(0, len(word_list) - 1)
+            word = word_list[word_number].upper()
         return word
 
     elif level == 2:
         while len(word) <= 5 or len(word) > 8:
-            word_number = random.randint(0, len(words) -1)
-            word = words[word_number].upper()
-        return word 
+            word_number = random.randint(0, len(word_list) - 1)
+            word = word_list[word_number].upper()
+        return word
 
     else:
         while len(word) <= 8:
-            word_number = random.randint(0, len(words) -1)
-            word = words[word_number].upper()
+            word_number = random.randint(0, len(word_list) - 1)
+            word = word_list[word_number].upper()
         return word
+
 
 def damage(level):
     if level == 1:
@@ -177,8 +182,10 @@ def damage(level):
         damage = 6
     return damage
 
+
 def get_hangman(max_wrong_guesses):
-    return int(max_wrong_guesses / (len(HANGMAN)-1 ))
+    return int(max_wrong_guesses / (len(HANGMAN) - 1))
+
 
 def guess_letter(word, encoded_word, level, wrong_guesses, guess_counter, already_guessed):
     az_check = string.ascii_letters
@@ -189,29 +196,31 @@ def guess_letter(word, encoded_word, level, wrong_guesses, guess_counter, alread
         guess = input("Please use A-Z characters.")
     for letter in range(0, len(word)):
         if word[letter] == guess.upper():
-            encoded_word = encoded_word[0:letter] + guess.upper() + encoded_word[letter+1:len(word)]
+            encoded_word = encoded_word[0:letter] + guess.upper() + encoded_word[letter + 1:len(word)]
             guess_counter += 1
     if guess_counter == 0 and guess.upper() not in already_guessed:
         already_guessed.append(guess.upper())
         wrong_guesses = wrong_guesses + damage(level)
 
     return wrong_guesses, encoded_word, already_guessed
+
+
 def main():
     ascii_banner = pyfiglet.figlet_format("Welcome to Hangman!")
-    print(bcolors.OKGREEN + ascii_banner + bcolors.ENDC)
+    print(BColors.OKGREEN + ascii_banner + BColors.ENDC)
     player_name = get_name()
     print("Welcome to Hangman,", player_name)
     level = get_level()
     word = get_word(words, level)
     original_word = word
     encoded_word = re.sub('[0-9a-zA-Z]', '_', word)
-    print(word) # Print selected word
-    print(encoded_word) # Print word with _
+    print(word)  # Print selected word
+    print(encoded_word)  # Print word with _
     already_guessed = []
-    max_wrong_guesses = 42 # Must be every 3
-    wrong_guesses = 0 # Starting value
-    guess_counter = 0 # Starting value for loop
-    print(HANGMAN[0]) # Starting Hangman
+    max_wrong_guesses = 42  # Must be every 3
+    wrong_guesses = 0  # Starting value
+    guess_counter = 0  # Starting value for loop
+    print(HANGMAN[0])  # Starting Hangman
     while wrong_guesses < max_wrong_guesses and "_" in encoded_word:
         hidden_letters = guess_letter(word, encoded_word, level, wrong_guesses, guess_counter, already_guessed)
         wrong_guesses = hidden_letters[0]
@@ -229,11 +238,6 @@ def main():
         decision = input("Congratulations, you've won the game! Do you want to try again? (y/n) ")
         if decision == "y":
             main()
-
-        
-        
-
-
 
 
 if __name__ == '__main__':

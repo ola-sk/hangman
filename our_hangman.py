3f2ca09896a6f171079f89217375dcb8200d44e5
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-05-25 03:25:09 +0200

    Introduce game rounds. Remember player's name and display randomised custom message when player either wins or fails. Line length formatting. included random.seed() before random.randint().

--- our_hangman.py
+++ our_hangman.py
@@ -123,10 +123,36 @@ def greet_player():
     print(BColors.OKGREEN + ascii_banner + BColors.ENDC)
     player_name = input("How can we call you? : ")
     print("Welcome to Hangman,", player_name + '!')
+    return player_name
+
+
+def welcome_back(player_name, is_win):
+    print()
+    random.seed()
+    random_message_index = random.randint(0, 2)
+    if is_win == 0:
+        if random_message_index == 0:
+            print(BColors.OKCYAN + "Welcome back",
+                  player_name + "! Let's try again! Try to save the man from cruelty!" + BColors.ENDC)
+        elif random_message_index == 1:
+            print(BColors.OKCYAN + "If you don't try, you never fail", player_name + "!" + BColors.ENDC)
+        elif random_message_index == 2:
+            print(BColors.OKCYAN + "There is no failure except in no longer trying! Let's keep going",
+                  player_name + "!" + BColors.ENDC)
+    elif is_win == 1:
+        if random_message_index == 0:
+            print(BColors.OKBLUE + "We can see you are good at it",
+                  player_name + "! Do you know names of all the countries on earth though?" + BColors.ENDC)
+        elif random_message_index == 1:
+            print(BColors.OKBLUE + "Winners are not afraid of losing. But losers are. Failure is part of the process "
+                                   "of success", player_name + "!" + BColors.ENDC)
+        elif random_message_index == 2:
+            print(BColors.OKBLUE + "Many innocent people are to be saved",
+                  player_name + "! Please be quick!" + BColors.ENDC)
 
 
 def get_level():
-    print("We are going to draw a country or a capital from a long list for you. ")
+    print("\nWe are going to draw a country or a capital from a long list for you. ")
     print("We have divided the words by a number of distinctive letters into 3 groups.")
     print(" 1 - Easy")
     print(" 2 - Medium")
@@ -172,6 +198,7 @@ def create_word_list(filepath="./countries-and-capitals.txt"):
 
 
 def draw_word_from_list(word_list, level):
+    random.seed()
     word_number = random.randint(0, len(word_list) - 1)
     word = word_list[word_number].upper()
 
@@ -216,39 +243,45 @@ def get_hangman(max_wrong_guesses, graphics_list):
     return int(max_wrong_guesses / (len(graphics_list) - 1))
 
 
-def main():
-    greet_player()
+def main(game_round, player_name=""):
+    if game_round == 'first':
+        player_name = greet_player()
+    elif game_round == 'next':
+        welcome_back(player_name, 1)
+    elif game_round == 'fail':
+        welcome_back(player_name, 0)
     level = get_level()
     word_base = create_word_list()
     word_to_guess = draw_word_from_list(word_base, level)
     original_word = word_to_guess
     encoded_word = re.sub('[0-9a-zA-Z]', '_', word_to_guess)
-    # TODO: remove `print(word_to_guess)` below (DEBUG ONLY)
-    print(word_to_guess)  # Print selected word
+    # print(word_to_guess)  # Print selected word
+    print(HANGMAN[0])  # Starting Hangman
     print(encoded_word)  # Print word with _
     already_guessed = []
     max_wrong_guesses = 42  # Must be every 3
     wrong_guesses = 0  # Starting value
     guess_counter = 0  # Starting value for loop
-    print(HANGMAN[0])  # Starting Hangman
     while wrong_guesses < max_wrong_guesses and "_" in encoded_word:
         hidden_letters = guess_letter(word_to_guess, encoded_word, level, wrong_guesses, guess_counter, already_guessed)
         wrong_guesses = hidden_letters[0]
         encoded_word = hidden_letters[1]
         already_guessed = hidden_letters[2]
-        index = int(wrong_guesses / get_hangman(max_wrong_guesses, HANGMAN))
-        print(HANGMAN[index])
+        hangman_graphics_index = int(wrong_guesses / get_hangman(max_wrong_guesses, HANGMAN))
+        print(HANGMAN[hangman_graphics_index])
         print(already_guessed)  # Print already guessed
         print(hidden_letters[1], hidden_letters[0])  # Print encoded word
     if wrong_guesses == max_wrong_guesses:
         decision = input("So sorry, you've failed! Do you want to play again? (y/n) ")
         if decision == "y":
-            main()
+            main("fail", player_name)
     else:
         decision = input("Congratulations, you've won the game! Do you want to try again? (y/n) ")
         if decision == "y":
-            main()
+            main("next", player_name)
 
 
 if __name__ == '__main__':
-    main()
+    main("first")
+
+# TODO: rozważ przypadek, gdzie wpiszemy spację jako zgadywaną literę. Obecnie wlicza spację, chcemy nie wliczać
